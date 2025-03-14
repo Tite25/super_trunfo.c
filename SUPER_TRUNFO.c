af6ba63d174c64c3956d9a2eafd449117b0b1b98
Tite25 <202501076572@alunos.estacio.br>
2025-03-14 07:21:57 +0000

    criei cartas super trunfo

---
+++ SUPER_TRUNFO.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+ 
+   int main () { 
+
+    // DECLARANDO VARIÁVEIS CARTA 
+
+       char carta = 'A';
+       char codigo = 'A01';       
+       char nome[20] = "FORTALEZA";
+       int populacao = 2429.000;
+       float area = 313.000;
+       float turismo = 50;
+
+       printf("DIGITE A CARTA:\n");
+       scanf("%c", &carta);
+       printf("CARTA 1\n");
+       printf("ESTADO: CEARA \n");
+       printf("CIDADE: FORTALEZA\n");
+       printf("CODIGO: A01\n");
+       printf("POPULAÇÃO: 2429.000\n");
+       printf("ÁREA: 313.000Km²\n");
+       printf("PONTOS TURISTICOS: 50 \n");
+       
+
+
+       printf("\nCARTA 2\n");
+       printf("ESTADO: CEARA \n");
+       printf("CIDADE: SOBRAL\n");
+       printf("CODIGO: A02\n");
+       printf("POPULAÇÃO: 203.023\n");
+       printf("ÁREA: 2.123Km²\n");
+       printf("PONTOS TURISTICOS: 35\n");
+
+
+
+
+
+   }
